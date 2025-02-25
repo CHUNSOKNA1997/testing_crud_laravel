d5e2b3fe9172cba120c9b7aee88fe74b3d46b0e4
CHUNSOKNA1997 <chun.sokna.1222@gmail.com>
2025-02-25 14:45:24 +0700

    succesfully testing making controller for Employee and make another controller for Student

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class StudentController extends Controller
+class EmployeeController extends Controller
 {
     public function index()
     {
-        return view('student.index');
+        return view('employees.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student', [StudentController::class, 'index']) -> name('students.index');
+
+Route::get('/employee', [EmployeeController::class, 'index']) -> name('employees.index');
